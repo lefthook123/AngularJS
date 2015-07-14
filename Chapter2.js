1e31aaeaecbc4dcc943193781c861538ca2ab620
lefthook123 <sf.jackwang@gmail.com>
2015-07-14 13:19:34 -0700

    Form Inputs

--- Chapter2.js
+++ Chapter2.js
@@ -1,7 +1,17 @@
-var myAppModule=angular.module('myApp',[]);
+var chp2Module = angular.module('chp2App',[]); 
 
-
-myAppModule.controller('Ch2Controller',function($scope)
-{
-	$scope.welcome={text='Hello!'};
+chp2Module.controller('Ch2Controller',function($scope){
+	$scope.welcome = {text: 'Welcome' };
 });
+
+
+chp2Module.controller('StartUpController',function($scope){
+	$scope.funding = { startingEstimate: 0, needed : 0};
+
+	$scope.computeNeeded = function() {
+		$scope.funding.needed = $scope.funding.startingEstimate*10;
+	};
+});
+
+
+
